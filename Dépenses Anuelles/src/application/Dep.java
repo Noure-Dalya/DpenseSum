8e30be89b99126348678cc72fad318d2e2c35eb0
noure-d-zobiri <noure-d-zobiri@noures-mbp>
2021-04-28 12:28:00 -0400

    Etudiant 1; Completer - Avril 28

--- Dépenses Anuelles/src/application/Dep.java
+++ Dépenses Anuelles/src/application/Dep.java
@@ -1,5 +1,56 @@
 package application;
 
-public class Dep {
+public class Dep
+
+{
+
+// En Double pour manipuler les donner en statistique
+
+private Double NC;
+private Double VC;
+private Double SC;
+private Double SupC;
+
+//Constructeur Vide:
+
+public Dep()
+{
+	this(null,null);
+}
+
+// Constructeur avec 2 parametre
+public Dep(Double NC,Double SupC) 
+{
+	this.NC=0.0;
+	this.VC=0.0;
+	this.SC=0.0;
+	this.SupC=0.0;
+}
+//Getters et Setters
+
+public Double getNC() {
+	return NC;
+}
+public void setNC(Double nC) {
+	NC = nC;
+}
+public Double getVC() {
+	return VC;
+}
+public void setVC(Double vC) {
+	VC = vC;
+}
+public Double getSC() {
+	return SC;
+}
+public void setSC(Double sC) {
+	SC = sC;
+}
+public Double getSupC() {
+	return SupC;
+}
+public void setSupC(Double supC) {
+	SupC = supC;
+}
 
 }
--- Dépenses Anuelles/src/application/DépensesController.java
+++ Dépenses Anuelles/src/application/DépensesController.java
@@ -1,5 +1,62 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class DépensesController {
 	
+// Table View
+    @FXML
+    private TableView<Dep> TV;
+  
+    //Colum dans le Table View
+    @FXML
+    private TableColumn<Dep,String> VC;
+
+    @FXML
+    private TableColumn<Dep, Double> SC;
+    
+    @FXML
+    private TableColumn<Dep, Double> NC;
+    
+    @FXML
+    private TableColumn<Dep, Double> EC;
+ 
+// Text Field
+    @FXML
+    private TextField SupText;
+
+    @FXML
+    private TextField Stext;
+
+    @FXML
+    private TextField HText;
+
+    @FXML
+    private TextField AText;
+
+// Button
+   
+    @FXML
+    private Button AB;
+
+    @FXML
+    private Button MB;
+
+    @FXML
+    private Button FB2;
+
+    @FXML
+    private Button EB;
+
+    @FXML
+    private Button RB;
+
+
+  
+
+
 }
